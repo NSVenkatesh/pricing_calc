8dc5e7e0bcae67d46e1943a759e7c617da903451
NSVenkatesh <85620459+NSVenkatesh@users.noreply.github.com>
2021-09-12 16:55:19 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#range").val("1")
+    $("#range").change(function(){
+       var  x=$("#range").val();
+        $("#user").html(x)
+        var calc=x*4.99;
+        $("#price").html("$ "+calc.toFixed(2));
+    })
+    $("#range").mousemove(function(){
+        var valueInput=$("#range").val();
+        var percent=valueInput/2;
+        var val="linear-gradient(to right,white 0%, white "+percent+"%, #75bbf8 "+percent+"%, #75bbf8 100%"
+        $("#range").css("background",val)
+    })
+})
+
